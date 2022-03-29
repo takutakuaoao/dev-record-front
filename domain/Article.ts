f87992eeb8c9a72968aa627de7edd45dbc962fa2
aochan <nakinagaraodoru1013@gmail.com>
2022-03-29 23:49:09 +0900

    【リファクタリング】記事詳細ページ

--- domain/Article.ts
+++ domain/Article.ts
@@ -1,5 +1,13 @@
 import moment from "moment";
+import markdownTheme from "../config/markdownTheme";
 import { Category } from "./Category";
+import { unified, UsePlugin } from "unified";
+import parser from "remark-parse";
+import remarkDirective from "remark-directive";
+import toHast from "remark-rehype";
+import compiler from "rehype-react";
+import { visit } from "unist-util-visit";
+import React from "react";
 
 export interface ArticleData {
   readonly id: string;
@@ -17,10 +25,22 @@ export interface ArticleDataList {
   readonly datalist: ArticleData[];
 }
 
+export interface ArticleUrlParam {
+  readonly categorySlug: string;
+  readonly articleSlug: string;
+}
+
 export class Article {
   readonly data: ArticleData;
   constructor(articleData: ArticleData) {
     this.data = articleData;
+  }
+
+  getUrlParam(): ArticleUrlParam {
+    return {
+      categorySlug: this.data.category.slug,
+      articleSlug: this.data.slug,
+    };
   }
 
   getArticleDate(): string {
@@ -41,6 +61,70 @@ export class Article {
     return "/" + this.data.category.slug + "/" + this.data.slug;
   }
 
+  getHeadTitle(): string {
+    const siteTitle = process.env.NEXT_PUBLIC_SITE_TITLE;
+
+    return this.data.title + " | " + siteTitle;
+  }
+
+  getPageDescription(): string {
+    return this.data.content.length > 150
+      ? (this.data.content.substr(0, 150) + "...").replace(/\r?\n/g, "")
+      : this.data.content.replace(/\r?\n/g, "");
+  }
+
+  parseMarkdown(): any {
+    const processor = this.getProcessorMarkdownParser();
+    const parsedMarkdown = processor.processSync(this.data.content).result;
+
+    return parsedMarkdown;
+  }
+
+  private getProcessorMarkdownParser(): UsePlugin {
+    return unified()
+      .use(parser)
+      .use(remarkDirective)
+      .use(this.addCustomizeGrammar)
+      .use(toHast)
+      .use(compiler, {
+        createElement: React.createElement,
+        components: markdownTheme,
+      });
+  }
+
+  private addCustomizeGrammar(): import("unified").Transformer {
+    return (tree) => {
+      visit(tree, (node: any) => {
+        if (node.type === "inlineCode") {
+          const data = node.data || (node.data = {});
+          data.hProperties = { inline: true };
+        }
+
+        if (
+          node.type === "textDirective" ||
+          node.type === "leafDirective" ||
+          node.type === "containerDirective"
+        ) {
+          if (
+            node.name === "info" ||
+            node.name === "memo" ||
+            node.name === "alert" ||
+            node.name === "wrap" ||
+            node.name === "m"
+          ) {
+            const data = node.data || (node.data = {});
+            const tagName = node.name;
+
+            data.hName = tagName;
+            data.hProperties = { attributes: node.attributes };
+          }
+
+          return;
+        }
+      });
+    };
+  }
+
   private isUpdatedArticle(): boolean {
     return this.data.updated_at !== null && this.data.updated_at !== "";
   }
@@ -52,10 +136,14 @@ export class Article {
   }
 }
 
+export const CreateArticleURL = (domain: string, articleParam: ArticleUrlParam): string => {
+  return domain + "/" + articleParam.categorySlug + "/" + articleParam.articleSlug;
+}
+
 export const ArticleListFactory = (datalist: ArticleData[]): Article[] => {
   return datalist.map((data) => ArticleFactory(data));
 };
 
-const ArticleFactory = (data: ArticleData): Article => {
+export const ArticleFactory = (data: ArticleData): Article => {
   return new Article(data);
 };
--- pages/[categorySlug]/[articleSlug].tsx
+++ pages/[categorySlug]/[articleSlug].tsx
@@ -1,173 +1,99 @@
-import { GetStaticPaths, GetStaticProps } from "next";
+import { GetStaticProps } from "next";
 import { ParsedUrlQuery } from "querystring";
 import { getApi } from "../../api/api";
-import markdownTheme from "../../config/markdownTheme";
-import moment from "moment";
 import Header from "../../components/organisms/header";
 import Subject from "../../components/organisms/article/subject";
 import FullImage from "../../components/atoms/fullImage";
-import { Box, Center } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Content from "../../components/organisms/article/content";
 import Footer from "../../components/organisms/footer";
 
-import { unified, UsePlugin } from "unified";
-import parser from "remark-parse";
-import remarkDirective from "remark-directive";
-import toHast from "remark-rehype";
-import compiler from "rehype-react";
 import React from "react";
-import { visit } from "unist-util-visit";
 import SiteHead from "../../components/molecules/siteHead";
+import {
+  Article,
+  ArticleData,
+  ArticleFactory,
+  ArticleListFactory,
+  ArticleUrlParam,
+  CreateArticleURL,
+} from "../../domain/Article";
 
-// interface Category {
-//   id: string;
-//   name: string;
-//   slug: string;
-// }
+interface Params extends ParsedUrlQuery {
+  categorySlug: string;
+  articleSlug: string;
+}
 
-// interface Article {
-//   id: string;
-//   type: number;
-//   category: Category;
-//   title: string;
-//   content: string;
-//   slug: string;
-//   main_img_url?: string;
-//   created_at: string;
-//   updated_at?: string;
-// }
+interface Props {
+  articleData: ArticleData;
+}
 
-// interface Params extends ParsedUrlQuery {
-//   categorySlug: string;
-//   articleSlug: string;
-// }
-
-// interface Props {
-//   article: Article;
-// }
-
-const Article = ({ article }) => {
-  const processor = unified()
-    .use(parser)
-    .use(remarkDirective)
-    .use(myRemarkPlugin)
-    .use(toHast)
-    .use(compiler, {
-      createElement: React.createElement,
-      components: markdownTheme,
-    });
-
-  const formattedDate = moment(
-    article.updated_at === null ? article.created_at : article.updated_at
-  ).format("YYYY.MM.DD");
-
-  const pageTitle = article.title + " | " + process.env.NEXT_PUBLIC_SITE_TITLE;
-
-  const pageDescription =
-    article.content.length > 150
-      ? (article.content.substr(0, 150) + "...").replace(/\r?\n/g, "")
-      : article.content.replace(/\r?\n/g, "");
-
-  const pageUrl =
-    process.env.NEXT_PUBLIC_SITE_URL +
-    "/" +
-    article.category.slug +
-    "/" +
-    article.slug;
-
-  const mainImgUrl =
-    article.main_img_url === null || article.main_img_url === ""
-      ? process.env.NEXT_PUBLIC_TEMP_DEFAULT_IMG_PATH
-      : article.main_img_url;
+const ArticlePage: React.VFC<Props> = ({ articleData }) => {
+  const article = ArticleFactory(articleData);
 
   return (
     <>
-      <SiteHead title={pageTitle} description={pageDescription} url={pageUrl} />
+      <SiteHead
+        title={article.getHeadTitle()}
+        description={article.getPageDescription()}
+        url={article.getPageUrl()}
+      />
       <Header />
-      <Box color={"gray.700"}>
-        <Box py={[4, 12]} px={[2, 0]} display={["block", "flex"]} justifyContent={["normal", "center"]}>
-          <Box maxW={"940px"} letterSpacing={0.8}>
-            <Box mb={[4, 12]}>
-              <Subject
-                category={article.category.name}
-                id={article.id}
-                title={article.title}
-                date={formattedDate}
-              />
-            </Box>
-            <Box mb={[6, 12]}>
-              <FullImage src={mainImgUrl} />
-            </Box>
-            <Content>{processor.processSync(article.content).result}</Content>
-          </Box>
-        </Box>
-      </Box>
+      <ContentArea article={article} />
       <Footer />
     </>
   );
 };
 
-function myRemarkPlugin(): import('unified').Transformer {
-  return (tree) => {
-    visit(tree, (node: any) => {
-      if (node.type === "inlineCode") {
-        const data = node.data || (node.data = {});
-        data.hProperties = { inline: true };
-      }
+const ContentArea: React.VFC<{ article: Article }> = ({ article }) => {
+  return (
+    <Box color={"gray.700"}>
+      <Box
+        py={[4, 12]}
+        px={[2, 0]}
+        display={["block", "flex"]}
+        justifyContent={["normal", "center"]}
+      >
+        <Box maxW={"940px"} letterSpacing={0.8}>
+          <ArticleContent article={article} />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
-      if (
-        node.type === "textDirective" ||
-        node.type === "leafDirective" ||
-        node.type === "containerDirective"
-      ) {
-        if (
-          node.name === "info" ||
-          node.name === "memo" ||
-          node.name === "alert" ||
-          node.name === "wrap" ||
-          node.name === "m"
-        ) {
-          const data = node.data || (node.data = {});
-          const tagName = node.name;
+const ArticleContent: React.VFC<{ article: Article }> = ({ article }) => {
+  return (
+    <>
+      <Box mb={[4, 12]}>
+        <Subject
+          category={article.data.category.name}
+          id={article.data.id}
+          title={article.data.title}
+          date={article.getArticleDate()}
+        />
+      </Box>
+      <Box mb={[6, 12]}>
+        <FullImage src={article.getMainImgUrl()} />
+      </Box>
+      <Content>{article.parseMarkdown()}</Content>
+    </>
+  );
+};
 
-          data.hName = tagName;
-          data.hProperties = { attributes: node.attributes };
-        }
-
-        return;
-      }
-    });
-  };
-}
-
-export const getStaticProps = async ({ params }) => {
-  const apiUri =
-    process.env.NEXT_PUBLIC_API_ARTICLE_INDEX +
-    "/" +
-    params.categorySlug +
-    "/" +
-    params.articleSlug;
-  const { state } = await getApi(apiUri, true);
+export const getStaticProps: GetStaticProps<Props, Params> = async ({
+  params,
+}) => {
+  const articleData = await FetchArticleDataFromApi(params);
 
   return {
-    props: { article: state.data.item },
+    props: { articleData: articleData },
   };
 };
 
 export const getStaticPaths = async () => {
-  const { state } = await getApi(
-    process.env.NEXT_PUBLIC_API_ARTICLE_INDEX,
-    true
-  );
-
-  const allParams = state.data.list.map((article) => {
-    return {
-      params: {
-        categorySlug: article.category.slug,
-        articleSlug: article.slug,
-      },
-    };
-  });
+  const articles = await FetchAllPublicArticlesFromApi();
+  const allParams = articles.map(CreateArticleParam);
 
   return {
     paths: allParams,
@@ -175,4 +101,25 @@ export const getStaticPaths = async () => {
   };
 };
 
-export default Article;
+const CreateArticleParam = (article: Article): { params: ArticleUrlParam } => {
+  return { params: article.getUrlParam() };
+};
+
+const FetchArticleDataFromApi = async (
+  param: ArticleUrlParam
+): Promise<ArticleData> => {
+  const apiBaseArticleUri = process.env.NEXT_PUBLIC_API_ARTICLE_INDEX;
+  const apiUri = CreateArticleURL(apiBaseArticleUri, param);
+  const { state } = await getApi(apiUri, true);
+
+  return state.data.item;
+};
+
+const FetchAllPublicArticlesFromApi = async () => {
+  const apiUrl = process.env.NEXT_PUBLIC_API_ARTICLE_INDEX;
+  const { state } = await getApi(apiUrl, true);
+
+  return ArticleListFactory(state.data.list);
+};
+
+export default ArticlePage;
